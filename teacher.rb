2223d7aa129450c508c5f539678710e0ca64a701
chrissiku <chrissiku5@gmail.com>
2022-08-22 17:34:20 +0200

    Fix linters

--- teacher.rb
+++ teacher.rb
@@ -2,14 +2,14 @@ require_relative 'person'
 
 # Class Teacher inherits from Person
 class Teacher < Person
-    # Constructor
-    def initialize(age, name = "Unknown", parent_permission = true, specialization)
-        super(name, age, parent_permission)
-        @specialization = specialization
-    end
+  # Constructor
+  def initialize(age, name = 'Unknown', parent_permission = true, specialization)
+    super(name, age, parent_permission)
+    @specialization = specialization
+  end
 
-    # Method can_use_services?
-    def can_use_services?
-        return true
-    end
+  # Method can_use_services?
+  def can_use_services?
+    true
+  end
 end
