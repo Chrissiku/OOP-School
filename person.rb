e5a99e46711abd1bae4acb82ac5b930f87f154c9
chrissiku <chrissiku5@gmail.com>
2022-08-22 17:12:55 +0200

    Create private method `is_of_age?`

--- person.rb
+++ person.rb
@@ -2,11 +2,17 @@
 class Person
     attr_reader :id
     attr_accessor :name, :age
-    
+
     # initialize the class person 
     def initialize(name = "Unknown", age,  parent_permission = true)
         @id = rand(1..1000)
         @name = name
         @age = age
     end
+    # Private method is_of_age?
+    def is_of_age?
+        @age >= 18
+    end
+    private :is_of_age?
+
 end
