40ad76040824ba60b1da97cf5b3ee7a81db0d299
chrissiku <chrissiku5@gmail.com>
2022-08-22 17:31:10 +0200

    Extends parent's constructor by adding `@specialization` and a parameter for it.

--- teacher.rb
+++ teacher.rb
@@ -2,4 +2,9 @@ require_relative 'person'
 
 # Class Teacher inherits from Person
 class Teacher < Person
+    # Constructor
+    def initialize(age, name = "Unknown", parent_permission = true, specialization)
+        super(name, age, parent_permission)
+        @specialization = specialization
+    end
 end
