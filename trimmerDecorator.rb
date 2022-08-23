1e561cf877faa538181e40e0d4c655fca143744e
chrissiku <chrissiku5@gmail.com>
2022-08-23 17:52:28 +0200

    Implement method `correct_name` that overridden the `Decorator`'s one

--- trimmerDecorator.rb
+++ trimmerDecorator.rb
@@ -1,4 +1,8 @@
 require_relative 'baseDecorator'
 
 class TrimmerDecorator < Decorator
+    # Method correct_name is overridden from Decorator class
+    def correct_name
+        @nemeable.correct_name[0..10]
+    end
 end
