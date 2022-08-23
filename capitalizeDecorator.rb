e94c27a64243546cfa42830f673e4f419ceb25cf
chrissiku <chrissiku5@gmail.com>
2022-08-23 17:48:27 +0200

    Implement Method `correct_name` is overridden from `Decorator` class

--- capitalizeDecorator.rb
+++ capitalizeDecorator.rb
@@ -1,4 +1,8 @@
 require 'baseDecorator'
 
 class CapitalizeDecorator < Decorator
+    #Method correct_name is overridden from Decorator class    
+    def correct_name
+        @nemeable.correct_name.capitalize
+    end
 end
