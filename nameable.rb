d228f9ede4eb142d30f6b158c8967f616abbc411
chrissiku <chrissiku5@gmail.com>
2022-08-23 17:30:21 +0200

    Implement a method `correct_name` that raise a `NotImplementedError`

--- nameable.rb
+++ nameable.rb
@@ -1,3 +1,7 @@
 #class Neamable
 class Nameable
+    # Method correct_name
+    def correct_name
+        raise NotImplementedError, "#{self.class} must implement abstract method '#{__method__}'"
+    end
 end
