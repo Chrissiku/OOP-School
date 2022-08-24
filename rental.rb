e70ccc07ca8ffb97cdf03e661352b4c717dcbf3d
chrissiku <chrissiku5@gmail.com>
2022-08-24 14:30:11 +0200

    Create the `belongs-to` side of `Rental` and `Book` (a rental belongs to a book)

--- rental.rb
+++ rental.rb
@@ -1,7 +1,8 @@
 class Rental
-    attr_accessor :date
+    attr_accessor :date, :book
 
-    def initialize(date)
+    def initialize(date, book)
         @date = date
+        @book = book
     end
 end
