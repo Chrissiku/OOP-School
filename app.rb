3bdec365b867b916b71bc379a0f7ea4a37c9952c
chrissiku <chrissiku5@gmail.com>
2022-08-25 00:09:15 +0200

    Implement the `create_person` method to select a type of a person to create

--- app.rb
+++ app.rb
@@ -31,4 +31,21 @@ class App
             puts "Title : #{book.title} | Author : #{book.author}"
         end
     end
+
+# ------------People------------------
+    # Create person method
+    def create_person
+        print "Do you want to create a student (1) or a teacher (2) ? [Input a number] : "
+        choice = gets.chomp
+        case choice
+        when "1"
+            puts "Student"
+        when "2"
+            puts "Teacher"
+        else
+            puts "Please enter a valid number [1 or 2]"
+            return
+        end
+        puts "Person created successfully"
+    end
 end
