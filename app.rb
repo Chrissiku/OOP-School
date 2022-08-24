53e65a497ee80a62db754104f30f11054ae62020
chrissiku <chrissiku5@gmail.com>
2022-08-24 22:47:51 +0200

    Implement `create_book` method in  order to create a new book

--- app.rb
+++ app.rb
@@ -1,7 +1,22 @@
+require_relative 'book'
 class App
     def initialize
-        @rentals = []
         @books = []
         @people = []
+        @rentals = []
+    end
+
+# ------------Books------------------
+
+    # Create book method
+    def create_book
+        print "Title : "
+        title = gets.chomp
+        print "Author : "
+        author = gets.chomp
+
+        #Book instance
+        book = Book.new(title, author)
+        @books.push(book)
     end
 end
