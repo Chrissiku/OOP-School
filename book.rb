3647bb534742f7c486aa7b04053a68661db05aab
chrissiku <chrissiku5@gmail.com>
2022-08-24 14:23:05 +0200

    Create the `has-many` side of `Book` and `Rental` (a book has many rentals)

--- book.rb
+++ book.rb
@@ -1,8 +1,14 @@
 class Book
+    attr_reader :rental
     attr_accessor :title, :author
 
     def initialize(title, author)
         @title = title
         @author = author
+    end    
+    
+    # Method add_rental
+    def add_rental(rental)
+        @rental.push(rental)
     end
 end
