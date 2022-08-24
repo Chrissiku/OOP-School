e7603d618bcd71d73f12249c2555eb0c8c6a372f
chrissiku <chrissiku5@gmail.com>
2022-08-24 23:23:46 +0200

    Create multiple option for the user

--- main.rb
+++ main.rb
@@ -1,10 +1,36 @@
 require './app'
 
 class Main_app
+    def self.options
+        puts "Welcome to School Library App!"
+        puts"\n"
+        puts "Please choose an option by entering a number :" 
+
+        # Menu options
+        @menu = {
+            "1" => "List all books",
+            "2" => "List all people",
+            "3" => "Create a person",
+            "4" => "Create a book",
+            "5" => "Create a rental",
+            "6" => "List all rentals for a given person\'s id",
+            "7" => "Exit"
+        }
+
+        # Display all menu options
+        @menu.each do |key, menu|
+            puts "#{key} - #{menu}"
+        end
+        # Get user selected option
+        Integer(gets.chomp)
+    end
 end
 
-
+# Method to call the app
 def main_call
     Main_app.new
 end
+
+# call the method to call the app
 main_call
+
