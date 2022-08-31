83d428cd86971ce6215c6dc3e8cafe3641631fb6
chrissiku <chrissiku5@gmail.com>
2022-08-31 20:54:41 +0200

    Initialize people file

--- data/store_info.rb
+++ data/store_info.rb
@@ -1,8 +1,13 @@
 module Store_data
     def load_file
+        #compute books file
         books_json_file = File.exist?('./store/books.json') ? File.read('./store/books.json') : '[]'
         book_hash = JSON.parse(books_json_file)
         update_books_hash(book_hash)
+
+        # compute people file
+        peaple_json_file = File.exist?('./store/people.json') ? File.read('./store/people.json') : '[]'
+        people_hash = JSON.parse(peaple_json_file)
     end
 
     def update_books_hash(book_h_array)
