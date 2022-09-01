d8607e322139ecc18679d77f60a2a960d65ffce9
chrissiku <chrissiku5@gmail.com>
2022-09-01 22:36:21 +0200

    Write test for the `title` and `author`

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -1,13 +1,29 @@
 require 'spec_helper'
 
 describe Book do
+    # Instantiate the Book class
     before :each do
         @book = Book.new "Title", "Author"
     end
 
+    # Test the instance of Book
     describe "#new" do
         it "Take 2 parameters and returns a Book object" do
             @book.should be_an_instance_of Book
+        end
+    end
+
+    # Tesst The title of the book
+    describe '#title' do
+        it "Returns the title of the book" do
+            @book.title.should eql "Title"
+        end
+    end
+
+    # Test the author of the book
+    describe '#author' do
+        it "Returns the author of the book" do
+            @book.author.should eql "Author"
         end
     end
 end
