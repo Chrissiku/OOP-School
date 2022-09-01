3d0204fc5e042085cc12b347b099f847f9adeae8
chrissiku <chrissiku5@gmail.com>
2022-09-01 10:56:45 +0200

    Load from and store rentals in `rentals.json` file

--- save_info.rb
+++ save_info.rb
@@ -1,24 +1,29 @@
 module Save_info
-    def save_info
-        books_hash = []
-        people_hash = []
-        to_hash(books_hash, people_hash)
-        File.open('./store/books.json', 'w') { |file| file.puts books_hash.to_json }
-        File.open('./store/people.json', 'w') { |f| f.puts people_hash.to_json }  
+  def save_info
+    books_hash = []
+    people_hash = []
+    rentals_hash = []
+    to_hash(books_hash, people_hash, rentals_hash)
+    File.open('./store/books.json', 'w') { |file| file.puts books_hash.to_json }
+    File.open('./store/people.json', 'w') { |f| f.puts people_hash.to_json }
+    File.open('./store/rentals.json', 'w') { |f| f.puts rentals_hash.to_json }
+  end
+
+  def to_hash(book_h, person_hash, rentals_hash)
+    # convert books to hash
+    @books.each do |book|
+      book_h << { title: book.title, author: book.author }
+    end
+    # convert people to hash
+    @people.each do |person|
+      person_hash << end
+      { class: person.class, name: person.name, age: person.age, id: person.id }
     end
 
-    def to_hash(book_h,person_hash)
-        # convert books to hash
-        @books.each do |book|
-            book_h << {title: book.title, author: book.author}
-        end
-        # convert people to hash
-        @people.each do |person|
-            if person.instance_of?(::Student)
-                person_hash << {class: person.class, name: person.name, age: person.age, id: person.id}
-            else
-                person_hash << {class: person.class, name: person.name, age: person.age, id: person.id}
-            end
-        end
+    # convert rentals to hash
+    @rentals.each do |rental|
+      rentals_hash << { date: rental.date, book_author: rental.book.author, rent_book: rental.book.title,
+                        rental_person: rental.person.name }
     end
+  end
 end
--- store_info.rb
+++ store_info.rb
@@ -1,31 +1,61 @@
 module Store_data
-    def load_file
-        #compute books file
-        books_json_file = File.exist?('./store/books.json') ? File.read('./store/books.json') : '[]'
-        book_hash = JSON.parse(books_json_file)
-        update_books_hash(book_hash)
-        people_json_file = File.exist?('./store/people.json') ? File.read('./store/people.json') : '[]'
-        people_hash = JSON.parse(people_json_file)
-        update_people_hash(people_hash)
+  def load_file
+    books_json_file = File.exist?('./store/books.json') ? File.read('./store/books.json') : '[]'
+    people_json_file = File.exist?('./store/people.json') ? File.read('./store/people.json') : '[]'
+    rental_json_file = File.exist?('./store/rentals.json') ? File.read('./store/rentals.json') : '[]'
+    book_hash = JSON.parse(books_json_file)
+    people_hash = JSON.parse(people_json_file)
+    rental_hash = JSON.parse(rental_json_file)
+    update_books_hash(book_hash)
+    update_people_hash(people_hash)
+    update_rental_hash(rental_hash)
+  end
 
-        # compute people file
-        
-    end
-    
-    # Update books hash
-    def update_books_hash(book_h_array)
-        return if book_h_array == []
-        book_h_array.each do |book_h|
-            @books << Book.new(book_h['title'], book_h['author'])
-        end
-    end
+  # Update books hash
+  def update_books_hash(book_h_array)
+    return if book_h_array == []
 
-    # Update people hash
-    def update_people_hash(people_h_array)
-        return if people_h_array == []
-        people_h_array.each do |people_h|
-            @people << Student.new(people_h['name'], people_h['age']) if people_h['class'] == 'Student'
-            @people << Teacher.new(people_h['name'], people_h['age'], people_h['specialization']) if people_h['class'] == 'Teacher'
-        end
+    book_h_array.each do |book_h|
+      @books << Book.new(book_h['title'], book_h['author'])
     end
+  end
+
+  # Update people hash
+  def update_people_hash(people_h_array)
+    return if people_h_array == []
+
+    people_h_array.each do |people_h|
+      @people << Student.new(people_h['name'], people_h['age']) if people_h['class'] == 'Student'
+      if people_h['class'] == 'Teacher'
+        @people << Teacher.new(people_h['name'], people_h['age'],
+                               people_h['specialization'])
+      end
+    end
+  end
+
+  # Check if book is available
+  def check_books_for_rent(rental)
+    @books.each do |book|
+      @book_r = book if rental['book_title'] == book.title
+    end
+  end
+
+  # Check if person is available
+  def check_people_to_rent(rental)
+    @books.each do |book|
+      @person_r = book if rental['book_title'] == book.title
+    end
+  end
+
+  # Update rental hash
+  def update_rental_hash(rental_h_array)
+    rental_h_array.each do |rental|
+      check_books_for_rent(rental)
+      check_people_to_rent(rental)
+      @people.each do |person|
+        @person_r = person if rental['person'] == person.name
+      end
+      @rentals << Rental.new(rental['date'], @book_r, @person_r) if @book_r && @person_r
+    end
+  end
 end
