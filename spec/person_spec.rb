c51bcbb23e8b98591602e530dc8f330bd78995b7
chrissiku <chrissiku5@gmail.com>
2022-09-01 23:43:29 +0200

    Fix linters

--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -1,42 +1,42 @@
 require 'spec_helper'
 
 describe Person do
-    before :each do
-        @person = Person.new 25, 'chris'
-    end
+  before :each do
+    @person = Person.new 25, 'chris'
+  end
 
-    # Test the instance of Book
-    describe "#new" do
-        it "Take parameters and returns a Person object" do
-            @person.should be_an_instance_of Person
-        end
-    end 
+  # Test the instance of Book
+  describe '#new' do
+    it 'Take parameters and returns a Person object' do
+      @person.should be_an_instance_of Person
+    end
+  end
 
-    # Test the age of the person
-    describe '#age' do
-        it 'Returns the age' do
-          expect(@person.age).to eql 25
-        end
+  # Test the age of the person
+  describe '#age' do
+    it 'Returns the age' do
+      expect(@person.age).to eql 25
     end
-    
-    # Test the name of the person
-    describe '#name' do
-        it 'Returns the name' do
-            expect(@person.name).to eql 'chris'
-        end
-    end
+  end
 
-    # Test the method can_use_services?
-    describe '#can_use_services?' do
-        it 'Returns true if the person\'s age > 18= or has the permission' do
-            expect(@person.can_use_services?).to be true
-        end
+  # Test the name of the person
+  describe '#name' do
+    it 'Returns the name' do
+      expect(@person.name).to eql 'chris'
     end
+  end
 
-    # Test the method rent_book
-    describe '#add_rental' do
-        it 'Returns a rental of the book' do
-            expect(@person.add_rental('2022/09/01', Person.new(01, 'chris'))).to be_an_instance_of Rental
-        end
+  # Test the method can_use_services?
+  describe '#can_use_services?' do
+    it 'Returns true if the person\'s age > 18= or has the permission' do
+      expect(@person.can_use_services?).to be true
     end
+  end
+
+  # Test the method rent_book
+  describe '#add_rental' do
+    it 'Returns a rental of the book' do
+      expect(@person.add_rental('2022/09/01', Person.new(0o1, 'chris'))).to be_an_instance_of Rental
+    end
+  end
 end
--- spec/student_spec.rb
+++ spec/student_spec.rb
@@ -1,42 +1,41 @@
 require 'spec_helper'
 
 describe Student do
+  # The student should inherit from Person
+  it 'Inherit from Person' do
+    expect(Student.superclass).to eq Person
+  end
 
-    # The student should inherit from Person
-    it 'Inherit from Person' do
-        expect(Student.superclass).to eq Person
-    end
+  # Instantiate the Student class.
+  before :each do
+    @student = Student.new 20, 2, 'chris'
+  end
 
-    # Instantiate the Student class.
-    before :each do
-        @student = Student.new 20, 2, 'chris'
+  # Test the isntance of Student
+  describe '#new' do
+    it 'Take 3 parameters and returns a Student object' do
+      @student.should be_an_instance_of Student
     end
+  end
 
-    # Test the isntance of Student
-    describe "#new" do
-        it "Take 3 parameters and returns a Student object" do
-            @student.should be_an_instance_of Student
-        end
+  # Test the age of the student
+  describe '#age' do
+    it 'Returns the age' do
+      expect(@student.age).to eql 20
     end
+  end
 
-    # Test the age of the student
-    describe '#age' do
-        it 'Returns the age' do
-            expect(@student.age).to eql 20
-        end
+  # Test the name of the student
+  describe '#name' do
+    it 'Returns the name' do
+      expect(@student.name).to eql 'chris'
     end
+  end
 
-    # Test the name of the student
-    describe '#name' do
-        it 'Returns the name' do
-            expect(@student.name).to eql 'chris'
-        end
+  # Test the classroom of the student
+  describe '#classroom' do
+    it 'Returns the classroom' do
+      expect(@student.classroom).to eql 2
     end
-
-    # Test the classroom of the student
-    describe '#classroom' do
-        it 'Returns the classroom' do
-            expect(@student.classroom).to eql 2
-        end
-    end
+  end
 end
